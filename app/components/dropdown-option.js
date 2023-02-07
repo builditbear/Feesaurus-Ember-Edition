ed3a176fa45255bc6a75a1d6ec566db9088a28e4
Brandon Chavez <brandon.na.chavez@pm.me>
2023-02-06 22:11:27 -0800

    Adjust indentation to 4 spaces via Prettier

--- app/components/dropdown-option.js
+++ app/components/dropdown-option.js
@@ -1,13 +1,12 @@
 import { action } from '@ember/object';
-import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class DropdownOption extends Component {
     @tracked patientResponsibility = 0;
-    
+
     @action
     updateChosenOption(event) {
-        this.patientResponsibility = (event.target.value || 0);
+        this.patientResponsibility = event.target.value || 0;
     }
 }
--- app/components/fee-calculator.js
+++ app/components/fee-calculator.js
@@ -1,19 +1,22 @@
-import Component from "@glimmer/component";
+import Component from '@glimmer/component';
 
 export default class FeeCalculator extends Component {
     lensOptions = [
         {
-            name: "Single Vision",
-            value: 30
-        }, {
-            name: "Bifocal",
-            value: 50
-        }, {
-            name: "Trifocal",
-            value: 100
-        }, {
-            name: "Progressive",
-            value: 200
-        }
-      ]
+            name: 'Single Vision',
+            value: 30,
+        },
+        {
+            name: 'Bifocal',
+            value: 50,
+        },
+        {
+            name: 'Trifocal',
+            value: 100,
+        },
+        {
+            name: 'Progressive',
+            value: 200,
+        },
+    ];
 }
